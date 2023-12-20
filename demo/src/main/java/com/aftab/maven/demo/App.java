c66107a25d51b1b6c79a12c9b0c64f2ea7a316d3
hp <hp@LAPTOP-97ES2BSF>
2023-12-20 16:13:32 +0530

    First Commit!!

---
+++ demo/src/main/java/com/aftab/maven/demo/App.java
@@ -0,0 +1,17 @@
+package com.aftab.maven.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello New World!!!";
+	}
+    public static void main( String[] args )
+    {
+    	App ob = new App();
+        System.out.println(ob.Hello() );
+    }
+}
